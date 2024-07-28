2a539b1be7e03bc7c33e7b9b6d0d7b326dd3c629
Ali Olleik <ali.oleik.2000@gmail.com>
2024-07-28 12:34:16 +0300

    users table done

--- index.js
+++ index.js
@@ -12,3 +12,24 @@ function getUsers(){
         });
       })
 }
+
+function fill(){
+    var selected_user=document.getElementById("users").value;
+    
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => {
+        var selected_object=data.find(el=>el.name===selected_user);
+        console.log(selected_user);
+        console.log(selected_object);
+        let _table=document.getElementById("info")
+      
+        let row=document.createElement("tr");
+        row.innerHTML=`
+         <td>${selected_object.name}</td>
+         <td>${selected_object.username}</td>
+        <td>${selected_object.email}</td>
+        `
+        _table.appendChild(row);
+      })
+}
